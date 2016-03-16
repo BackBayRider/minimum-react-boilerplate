bfb3df6660dd8b91b8fc0dadc5364774e8e77eea
Oleg Kalyta <2xyligan@gmail.com>
2016-03-16 23:30:47 +0200

    sample

--- src/Component.js
+++ src/Component.js
@@ -1,92 +1,36 @@
 import React, { Component, PropTypes } from 'react';
-import { reduxForm } from 'redux-form';
-export const fields = ['firstName', 'lastName', 'email', 'sex', 'favoriteColor', 'employed', 'notes'];
+import { reduxForm, addArrayValue } from 'redux-form';
+export const fields = ['arr[].name'];
 
-@reduxForm({ form: 'simple', fields })
+@reduxForm({ form: 'simple', fields }, undefined, { addValue: addArrayValue })
 export default class SimpleForm extends Component {
   static propTypes = {
+    addValue: PropTypes.func.isRequired,
     fields: PropTypes.object.isRequired,
-    handleSubmit: PropTypes.func.isRequired,
-    resetForm: PropTypes.func.isRequired,
-    submitting: PropTypes.bool.isRequired,
   };
+
+  componentDidMount() {
+    this.props.addValue('simple', 'arr');
+  }
 
   render() {
     const {
-      fields: { firstName, lastName, email, sex, favoriteColor, employed, notes },
-      handleSubmit,
-      resetForm,
-      submitting,
+      fields: { arr },
+      addValue,
+      dirty,
     } = this.props;
-    return (<form onSubmit={handleSubmit}>
+    return (<form>
         <div>
-          <label>First Name</label>
-          <div>
-            <input type="text" placeholder="First Name" {...firstName} />
-          </div>
         </div>
         <div>
-          <label>Last Name</label>
-          <div>
-            <input type="text" placeholder="Last Name" {...lastName} />
-          </div>
+          {arr.map((ar, index) => <div key={index}>
+            <input type="text" {...ar.name} placeholder={index} />
+          </div>)}
         </div>
-        <div>
-          <label>Email</label>
-          <div>
-            <input type="email" placeholder="Email" {...email} />
-          </div>
-        </div>
-        <div>
-          <label>Sex</label>
-          <div>
-            <label>
-              <input type="radio" {...sex} value="male" checked={sex.value === 'male'} /> Male
-            </label>
-            <label>
-              <input type="radio" {...sex} value="female" checked={sex.value === 'female'} /> Female
-            </label>
-          </div>
-        </div>
-        <div>
-          <label>Favorite Color</label>
-          <div>
-            <select
-              {...favoriteColor}
-              value={favoriteColor.value || ''}  // required syntax for reset form to work
-              // undefined will not change value to first empty option
-              // when resetting
-            >
-              <option></option>
-              <option value="ff0000">Red</option>
-              <option value="00ff00">Green</option>
-              <option value="0000ff">Blue</option>
-            </select>
-          </div>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" {...employed} /> Employed
-          </label>
-        </div>
-        <div>
-          <label>Notes</label>
-          <div>
-            <textarea
-              {...notes}
-              value={notes.value || ''} // required for reset form to work (only on textarea's)
-              // see: https://github.com/facebook/react/issues/2533
-            />
-          </div>
-        </div>
-        <div>
-          <button type="submit" disabled={submitting}>
-            {submitting ? <i /> : <i />} Submit
-          </button>
-          <button type="button" disabled={submitting} onClick={resetForm}>
-            Clear Values
-          </button>
-        </div>
+        <button type="button" onClick={() => addValue('simple', 'arr')}>Add</button>
+        <button type="button" onClick={() => arr.removeField()}>Remove</button>
+        <br />
+        Dirty: {dirty.toString()}
       </form>
     );
   }
